3bc2a7289406b9f3e46d1c03af2b5f4a68dcd35f
eldon0508 <66116835+eldon0508@users.noreply.github.com>
2023-06-01 14:17:34 +0100

    initial

---
+++ database.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'pos',
+    user: 'root',
+    password: '',
+    multipleStatements: true,
+});
+
+connection.connect(function (error) {
+    if (error) {
+        throw error;
+    }
+    else {
+        console.log('MySQL Database is connected Successfully');
+    }
+});
+
+module.exports = connection;
